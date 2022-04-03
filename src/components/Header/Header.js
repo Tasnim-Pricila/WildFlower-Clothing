7d0629625d0de4ba6b2b0c19adaac669878b86c4
unknown <tasnimtanzim55@gmail.com>
2022-04-04 01:54:59 +0600

    Menubar and routing added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='w-full border-2 text-xl text-center font-semibold pt-6 pb-4 uppercase'>
+            <Link to ="/" className='mr-10 nav-link'> Home </Link>
+            <Link to ="/reviews" className='mr-10 nav-link'> Reviews </Link>
+            <Link to ="/dashboard" className='mr-10 nav-link'> Dashboard </Link>
+            <Link to ="/blogs" className='mr-10 nav-link'> Blogs </Link>
+            <Link to ="/about" className='mr-10 nav-link'> About </Link>
+        </div>
+    );
+};
+
+export default Header;
