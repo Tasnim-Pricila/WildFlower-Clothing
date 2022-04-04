cee8161b9116a41e45b225a40c502cf1ac124859
unknown <tasnimtanzim55@gmail.com>
2022-04-04 19:35:03 +0600

    Dashboard page Done

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,10 +1,92 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Area, AreaChart, Bar, BarChart, CartesianGrid, Legend, Line, LineChart, Pie, PieChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Dashboard = () => {
+
+    const [chart, setChart] = useState([]);
+    useEffect(() => {
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data => setChart(data))
+    },[])
+
     return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
+        <>
+            <div className='grid grid-cols-2 gap-x-36 px-36'>
+            
+                <div className='mt-10'>
+                    <LineChart 
+                        width={500} 
+                        height={300} 
+                        data={chart}
+                        margin={{ 
+                            top: 5, 
+                            right: 30, 
+                            left: 20, 
+                            bottom: 5 
+                        }}> 
+                        <CartesianGrid strokeDasharray="3 3"> </CartesianGrid>
+                        <XAxis dataKey={'month'}> </XAxis>
+                        <YAxis> </YAxis>
+                        <Tooltip> </Tooltip>
+                        <Legend></Legend>
+                        <Line dataKey={'sell'} stroke="purple" type='monotone'> </Line>
+                    </LineChart>
+                </div>
+                
+                <div className='mt-10'>               
+                    <AreaChart
+                            width={500}
+                            height={250}
+                            data={chart}
+                            margin={{
+                                top: 10,
+                                right: 30,
+                                left: 0,
+                                bottom: 0,
+                            }} >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="month" />
+                            <YAxis />
+                            <Tooltip />
+                            <Area type="monotone" dataKey="investment" stackId="1" stroke="#8884d8" fill="#8884d8" />
+                            <Area type="monotone" dataKey="revenue" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
+                    </AreaChart>   
+                </div>
+                <div className='mt-10'>
+                    
+                        <BarChart
+                            width={500}
+                            height={300}
+                            data={chart}
+                            margin={{
+                                top: 5,
+                                right: 30,
+                                left: 20,
+                                bottom: 5,
+                            }}
+                            >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="month" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Bar dataKey="investment" fill="#8884d8" stackId="a" />
+                            <Bar dataKey="revenue" fill="#82ca9d" stackId="a" />
+                        </BarChart>
+                    
+                </div>
+
+                <div className='mt-10'>   
+                    <ResponsiveContainer>
+                        <PieChart width={500} height={500}>
+                            <Pie data={chart} dataKey="investment" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" />
+                            <Pie data={chart} dataKey="revenue" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label />
+                        </PieChart>   
+                    </ResponsiveContainer> 
+                </div>
+            </div>
+        </>
     );
 };
 
--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const Review = (props) => {
-    const {name, description, img} = props.review;
+    const {name, description, img, rating} = props.review;
     return (
         <div className='flex flex-col border p-10 rounded-lg h-[450px]'>
             <img src={img} alt="" className='w-[70px] h-[70px] rounded-full object-cover mx-auto'/>
             <p className='font-bold mt-8 mb-6'> {name} </p>
+            <p className='mb-6'><b>Rating:</b> {rating} </p>
             <p> {description} </p>
         </div>
     );
